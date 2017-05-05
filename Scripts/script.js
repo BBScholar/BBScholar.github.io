9b62633e4b9ba0bea35ac8dd2797584e8e3fca08
Ben Scholar <bens@digitalman.com>
2017-05-05 11:30:52 -0700

    23

--- Scripts/script.js
+++ Scripts/script.js
@@ -2,10 +2,14 @@ $(document).ready(function () {
 
 	$('.img1').mouseenter(function () {
 		console.log('hover');
-
-		
 	})
 
-
+	$('#dank').on('click', function () {
+		var val1 = $('#dank').val();
+		console.log(val1);
+		if (val1 === 'on') {
+			
+		}
+	})
 
 })

ced543d35472fce99acbf2bc683a4d9c27e28a9f
Ben Scholar <bens@digitalman.com>
2017-05-11 11:29:01 -0700

    23

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,16 +1,28 @@
 $(document).ready(function () {
-	
+
 	var dank_mode = false;
 	$('#dank').on('click', function () {
 		if ($('#dank').is(":checked")) {
   			var result = confirm('Enter this mode at your own risk! Do you wish to continue?');
   			if (result) {
-  				console.log(true);
+  				console.log('dank: on');
+          $('h1').css('animation', 'rotate1 2s ease-in-out infinite'); 
+          $('h2').css('animation', 'rotate1 3s linear infinite');
+          $('h3').css('animation', 'up-down 1s linear infinite');
+          $('p').css('animation', '');
+
+
 
   			} else {
-  				console.log(false);
+  				console.log('dank: off');
+          $('#dank').prop('checked', false);
   			}
-		}
+		} else {
+      console.log('dank: off');
+      $('h1').css('animation', 'none'); 
+      $('h2').css('animation', 'none'); 
+
+    }
 	})
 
 })

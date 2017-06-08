5608b55dca6bb6c51fc7cba57f7a84b64c44ab26
Ben Scholar <bens@digitalman.com>
2017-06-08 10:39:31 -0700

    45

--- Scripts/easterEgg.js
+++ Scripts/easterEgg.js
@@ -2,6 +2,17 @@ var clicked = [];
 // 0 = Off
 // 1 = On
 
+var first = 0;
+if (localStorage.first = 1) {
+  first = 1;
+}
+console.log(first);
+
+if (first = 0) {
+  alert('Find all 5 easter eggs to play a secret game! Easter eggs are highlighted when hovered over. Click them to activate them.');
+  localStorage.first = 1;
+}
+
 clicked[0] = Number(localStorage.E1);
 clicked[1] = Number(localStorage.E2);
 clicked[2] = Number(localStorage.E3);
@@ -17,7 +28,7 @@ for (var i = 0; i < 5; i++) {
 function easter (number) {
   console.log('easter ' + number);
   alert('You found an Easter Egg! Number ' + number + ' of ' + clicked.length);
-  clicked[number] = 1;
+  clicked[number - 1] = 1;
   checkEaster();
 }
 
@@ -25,7 +36,7 @@ function checkEaster () {
   var count = 0
   for (var i = 0; i < clicked.length; i++) {
     if (clicked[i] == 1) {
-      count++
+      count++;
     }
   }
   if (count == clicked.length) {
